f15d143265eac1f5a38d854627ae729e7263c865
efirmansyah <endarfirmansyah@gmail.com>
2020-12-30 22:12:04 +0700

    Program for checking number

---
+++ checknumber.py
@@ -0,0 +1,8 @@
+#Pythhon program for checking numbers, (Positive, Negative, Zero)
+num = float(input("Enter a Number: "))
+if num > 0:
+    print("is Positive Number")
+elif num == 0:
+    print("is Zero")
+else:
+    print("is Negative Number")

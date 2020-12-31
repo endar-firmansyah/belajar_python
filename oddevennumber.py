ec01c7dacfc2191e9b0eb3c0fc56ee677a6cdc9f
efirmansyah <endarfirmansyah@gmail.com>
2020-12-31 14:02:33 +0700

    Program to determine odd or even number

---
+++ oddevennumber.py
@@ -0,0 +1,10 @@
+# Python program to check if the input number is odd or even.
+# A number is even if division by given 2 gives a remainder of 0.
+# If the remainder is 1, it is an odd number
+#div = int(input("Input a number: "))
+div = 79
+if (div % 2) == 0:
+    print("{0} It is even Number".format(div))
+else:
+    print("{0} It is an Odd Number".format(div))
+
